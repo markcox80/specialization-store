90dd084fad06f27d3b14ca2c7cc0ab14f14f8e74
Mark Cox <markcox80@gmail.com>
2015-11-13 07:23:56 +1000

    Added new function DETERMINE-FORM-TYPE.

--- src/common.lisp
+++ src/common.lisp
@@ -5,3 +5,23 @@
 					       lambda-list
 					       body))
 
+(defun the-form-p (form)
+  (and (listp form)
+       (eql 'the (first form))
+       (= 3 (length form))))
+
+(defun determine-form-type (form env)
+  (let ((form (macroexpand form env)))
+    (cond
+      ((constantp form env)
+       (type-of (introspect-environment:constant-form-value form env)))
+      ((the-form-p form)
+       (second form))
+      ((symbolp form)
+       (or (introspect-environment:variable-type form env)
+	   t))
+      ((and form (listp form))
+       (or (car (last (introspect-environment:function-type (first form) env)))
+	   t))
+      (t
+       (error "Do not know how to process form.")))))
