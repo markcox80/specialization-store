673248dd42dff6a9d1a8f5f7de48f14602a415ae
Mark Cox <markcox80@gmail.com>
2016-06-22 13:39:36 +1000

    Invoke process-test-streams.

--- tests/asdf.lisp
+++ tests/asdf.lisp
@@ -3,6 +3,6 @@
 (defmethod asdf:perform ((op asdf:test-op) (c (eql (asdf:find-system "specialization-store-tests"))))
   (5am:run! 'all-tests)
 
-  (format t "~&;;;; Running syntax layer tests.~%")
-  (process-syntax-layer-tests)
+  (format t "~&;;;; Running test streams.~%")
+  (process-test-streams)
   (format t "~&;;;; Done.~%"))
