a574b41af583a5f3a471e23bad7da2ea89d40a2f
Mark Cox <markcox80@gmail.com>
2017-04-23 07:19:23 +1000

    Fixed parse-store-lambda-list test.

--- tests/lambda-lists.lisp
+++ tests/lambda-lists.lisp
@@ -6,40 +6,45 @@
 
 (test parse-store-lambda-list
   (flet ((do-trial (store-lambda-list req opt rest key? keys allow-other-keys?)
-	   (let ((store-parameters (parse-store-lambda-list store-lambda-list)))
-	     (is-true (typep store-parameters 'store-parameters))
-	     (is (and (equal req (required-parameters store-parameters))
-		      (equal opt (optional-parameters store-parameters))
-		      (eql rest (rest-parameter store-parameters))
-		      (eql key? (keyword-parameters-p store-parameters))
-		      (equal keys (keyword-parameters store-parameters))
-		      (eql allow-other-keys? (allow-other-keys-p store-parameters)))
-		 "Failed to parse store lambda list ~W correctly." store-lambda-list))))
+           (let ((store-parameters (parse-store-lambda-list store-lambda-list)))
+             (is-true (typep store-parameters 'store-parameters))
+             (is (and (equal req (mapcar #'parameter-lambda-list-specification (required-parameters store-parameters)))
+                      (equal opt (when (optional-parameters-p store-parameters)
+                                   (mapcar #'parameter-lambda-list-specification (optional-parameters store-parameters))))
+                      (eql rest (when (rest-parameter-p store-parameters)
+                                  (parameter-lambda-list-specification (rest-parameter store-parameters))))
+                      (eql key? (keyword-parameters-p store-parameters))
+                      (equal keys (when (keyword-parameters-p store-parameters)
+                                    (mapcar #'parameter-lambda-list-specification (keyword-parameters store-parameters))))
+                      (eql allow-other-keys? (allow-other-keys-p store-parameters)))
+                 "Failed to parse store lambda list ~W correctly." store-lambda-list))))
     (let ((required '((nil . nil)
-		      ((a) . (a))
-		      ((a b) . (a b))))
-	  (optional '((nil . nil)
-		      ((&optional) . nil)
-		      ((&optional d) . ((d nil)))
-		      ((&optional (e 5) d) . ((e 5) (d nil)))))
-	  (rest '((nil . nil)
-		  ((&rest args) . args)))
-	  (keys '((nil nil nil nil)
-		  ((&key) nil t nil)
-		  ((&key m) ((:m m nil)) t nil)
-		  ((&key m (n 2)) ((:m m nil) (:n n 2)) t nil)
-		  ((&key (o 5) &allow-other-keys) ((:o o 5)) t t)
-		  ((&key ((:o p) 5)) ((:o p 5)) t nil))))
+                      ((a) . (a))
+                      ((a b) . (a b))))
+          (optional '((nil . nil)
+                      ((&optional) . nil)
+                      ((&optional d) . (d))
+                      ((&optional (e 5) d) . ((e 5) d))
+                      ((&optional (d 5 dp) (e d ep)) . ((d 5 dp) (e d ep)))))
+          (rest '((nil . nil)
+                  ((&rest args) . args)))
+          (keys '((nil nil nil nil)
+                  ((&key) nil t nil)
+                  ((&key m) (m) t nil)
+                  ((&key m (n 2)) (m (n 2)) t nil)
+                  ((&key (o 5) &allow-other-keys) ((o 5)) t t)
+                  ((&key ((:o p) 5)) (((:o p) 5)) t nil)
+                  ((&key (m 5 mp) (n m np)) ((m 5 mp) (n m np)) t nil))))
       (loop for (r-list . r) in required do
-	   (loop for (o-list . o) in optional do
-		(loop for (rest-list . rest-var) in rest do
-		     (loop for (keys-list k keys? allow-other-keys?) in keys do
-			  (let ((lambda-list (append r-list o-list rest-list keys-list)))
-			    (do-trial lambda-list r o rest-var keys? k allow-other-keys?)))))))))
+           (loop for (o-list . o) in optional do
+                (loop for (rest-list . rest-var) in rest do
+                     (loop for (keys-list k keys? allow-other-keys?) in keys do
+                          (let ((lambda-list (append r-list o-list rest-list keys-list)))
+                            (do-trial lambda-list r o rest-var keys? k allow-other-keys?)))))))))
 
 (test parse-store-lambda-list/invalid-store-lambda-lists
   (flet ((trial (store-lambda-list)
-	   (signals parse-store-lambda-list-error (parse-store-lambda-list store-lambda-list))))
+           (signals parse-store-lambda-list-error (parse-store-lambda-list store-lambda-list))))
     ;; Invalid markers
     (trial '(&rest))
     (trial '(&rest &rest))
@@ -68,9 +73,9 @@
 
 (test parse-store-lambda-list/duplicate-keywords
   (labels ((do-trial (lambda-list)
-	     (signals parse-store-lambda-list-error (parse-store-lambda-list lambda-list))))
+             (signals parse-store-lambda-list-error (parse-store-lambda-list lambda-list))))
     (macrolet ((trial (lambda-list)
-		 `(do-trial ',lambda-list)))
+                 `(do-trial ',lambda-list)))
       (trial (&key a a))
       (trial (&key a a b))
       (trial (&key a (a 1)))
@@ -81,43 +86,43 @@
 
 (test parse-specialization-lambda-list
   (flet ((do-trial (specialization-lambda-list req opt rest key? keys allow-other-keys?)
-	   (let ((specialization-parameters (parse-specialization-lambda-list specialization-lambda-list)))
-	     (is-true (typep specialization-parameters 'specialization-parameters))
-	     (is (and (equal req (required-parameters specialization-parameters))
-		      (equal opt (optional-parameters specialization-parameters))
-		      (eql rest (rest-parameter specialization-parameters))
-		      (eql key? (keyword-parameters-p specialization-parameters))
-		      (equal keys (keyword-parameters specialization-parameters))
-		      (eql allow-other-keys? (allow-other-keys-p specialization-parameters)))
-		 "Failed to parse specialization lambda list ~W correctly." specialization-lambda-list))))
+           (let ((specialization-parameters (parse-specialization-lambda-list specialization-lambda-list)))
+             (is-true (typep specialization-parameters 'specialization-parameters))
+             (is (and (equal req (required-parameters specialization-parameters))
+                      (equal opt (optional-parameters specialization-parameters))
+                      (eql rest (rest-parameter specialization-parameters))
+                      (eql key? (keyword-parameters-p specialization-parameters))
+                      (equal keys (keyword-parameters specialization-parameters))
+                      (eql allow-other-keys? (allow-other-keys-p specialization-parameters)))
+                 "Failed to parse specialization lambda list ~W correctly." specialization-lambda-list))))
     (let ((required '((nil . nil)
-		      ((a) . ((a t)))
-		      ((a b) . ((a t) (b t)))
-		      ((a (b integer)) . ((a t) (b integer)))))
-	  (optional '((nil . nil)
-		      ((&optional) . nil)
-		      ((&optional d) . ((d nil nil)))
-		      ((&optional (e 5) d) . ((e 5 nil) (d nil nil)))
-		      ((&optional (e 6 e-p)) . ((e 6 e-p)))))
-	  (rest '((nil . nil)
-		  ((&rest args) . args)))
-	  (keys '((nil nil nil nil)
-		  ((&key) nil t nil)
-		  ((&key m) ((:m m nil nil)) t nil)
-		  ((&key m (n 2)) ((:m m nil nil) (:n n 2 nil)) t nil)
-		  ((&key (o 5) &allow-other-keys) ((:o o 5 nil)) t t)
-		  ((&key (m 10 m-p)) ((:m m 10 m-p)) t nil)
-		  ((&key ((:k m) 10 m-p) &allow-other-keys) ((:k m 10 m-p)) t t))))
+                      ((a) . ((a t)))
+                      ((a b) . ((a t) (b t)))
+                      ((a (b integer)) . ((a t) (b integer)))))
+          (optional '((nil . nil)
+                      ((&optional) . nil)
+                      ((&optional d) . ((d nil nil)))
+                      ((&optional (e 5) d) . ((e 5 nil) (d nil nil)))
+                      ((&optional (e 6 e-p)) . ((e 6 e-p)))))
+          (rest '((nil . nil)
+                  ((&rest args) . args)))
+          (keys '((nil nil nil nil)
+                  ((&key) nil t nil)
+                  ((&key m) ((:m m nil nil)) t nil)
+                  ((&key m (n 2)) ((:m m nil nil) (:n n 2 nil)) t nil)
+                  ((&key (o 5) &allow-other-keys) ((:o o 5 nil)) t t)
+                  ((&key (m 10 m-p)) ((:m m 10 m-p)) t nil)
+                  ((&key ((:k m) 10 m-p) &allow-other-keys) ((:k m 10 m-p)) t t))))
       (loop for (r-list . r) in required do
-	   (loop for (o-list . o) in optional do
-		(loop for (rest-list . rest-var) in rest do
-		     (loop for (keys-list k keys? allow-other-keys?) in keys do
-			  (let ((lambda-list (append r-list o-list rest-list keys-list)))
-			    (do-trial lambda-list r o rest-var keys? k allow-other-keys?)))))))))
+           (loop for (o-list . o) in optional do
+                (loop for (rest-list . rest-var) in rest do
+                     (loop for (keys-list k keys? allow-other-keys?) in keys do
+                          (let ((lambda-list (append r-list o-list rest-list keys-list)))
+                            (do-trial lambda-list r o rest-var keys? k allow-other-keys?)))))))))
 
 (test parse-specialization-lambda-list/invalid-specialization-lambda-lists
   (flet ((trial (specialization-lambda-list)
-	   (signals parse-specialization-lambda-list-error (parse-specialization-lambda-list specialization-lambda-list))))
+           (signals parse-specialization-lambda-list-error (parse-specialization-lambda-list specialization-lambda-list))))
     ;; Invalid markers
     (trial '(&rest))
     (trial '(&rest &rest))
@@ -143,9 +148,9 @@
 
 (test parse-specialization-lambda-list/duplicate-keywords
   (labels ((do-trial (lambda-list)
-	     (signals parse-specialization-lambda-list-error (parse-specialization-lambda-list lambda-list))))
+             (signals parse-specialization-lambda-list-error (parse-specialization-lambda-list lambda-list))))
     (macrolet ((trial (lambda-list)
-		 `(do-trial ',lambda-list)))
+                 `(do-trial ',lambda-list)))
       (trial (&key a a))
       (trial (&key a a b))
       (trial (&key a (a 1)))
@@ -154,9 +159,9 @@
 
 (test parse-specialization-lambda-list/duplicate-variables
   (labels ((do-trial (lambda-list)
-	     (signals parse-specialization-lambda-list-error (parse-specialization-lambda-list lambda-list))))
+             (signals parse-specialization-lambda-list-error (parse-specialization-lambda-list lambda-list))))
     (macrolet ((trial (lambda-list)
-		 `(do-trial ',lambda-list)))
+                 `(do-trial ',lambda-list)))
       (trial (a a))
       (trial (a &optional a))
       (trial (a &optional (a nil)))
@@ -189,16 +194,16 @@
 
 (test congruent-specialization-parameters
   (flet ((do-trial (expected store-lambda-list specialization-lambda-list)
-	   (let* ((store-parameters (parse-store-lambda-list store-lambda-list))
+           (let* ((store-parameters (parse-store-lambda-list store-lambda-list))
                   (specialization-parameters (parse-specialization-lambda-list specialization-lambda-list))
                   (congruence (congruent-parameters-p store-parameters specialization-parameters)))
-	     (if expected
-		 (is-true congruence)
-		 (is-false congruence)))))
+             (if expected
+                 (is-true congruence)
+                 (is-false congruence)))))
     (macrolet ((true (store-lambda-list specialization-lambda-list)
-		 `(do-trial t ',store-lambda-list ',specialization-lambda-list))
-	       (false (store-lambda-list specialization-lambda-list)
-		 `(do-trial nil ',store-lambda-list ',specialization-lambda-list)))
+                 `(do-trial t ',store-lambda-list ',specialization-lambda-list))
+               (false (store-lambda-list specialization-lambda-list)
+                 `(do-trial nil ',store-lambda-list ',specialization-lambda-list)))
       ;; Positional
       (true (a) (b))
       (false (a) ())
@@ -534,7 +539,7 @@
         (destructuring-bind (name a b c) (funcall function '(test 1) nil)
           (is (eql 'test name))
           (is (eql 1 a))
-          (is-true (equal b c)))        
+          (is-true (equal b c)))
 
         (destructuring-bind (name a b c) (funcall function '(test 1 2) nil)
           (is (eql 'test name))
@@ -555,19 +560,19 @@
       (let* ((completion-function (compile nil lambda-form))
              (function (funcall completion-function (lambda (form env completed-form)
                                                       (declare (ignore form env))
-                                                      completed-form))))        
+                                                      completed-form))))
         (is (= 1 (length globals)))
         (destructuring-bind (name a b c) (funcall function '(test 1) nil)
           (is (eql 'test name))
           (is (eql 1 a))
-          (is-true (equal b c)))        
-        
+          (is-true (equal b c)))
+
         (destructuring-bind (name a b c) (funcall function '(test 1 2) nil)
           (is (eql 'test name))
           (is (eql 1 a))
           (is (eql 2 b))
           (is (eql 2 c)))
-        
+
         (destructuring-bind (name a b c) (funcall function '(test 1 2 3) nil)
           (is (eql 'test name))
           (is (eql 1 a))
