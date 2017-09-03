8e13cff474b25310d79206f91dd33ce39ae6f6e9
Mark Cox <markcox80@gmail.com>
2017-09-03 21:31:35 +1000

    Rewrote the processing of sandbox tests.

--- tests/test-streams.lisp
+++ tests/test-streams.lisp
@@ -20,56 +20,101 @@
   `(progn
      ,@body))
 
-(defun process-test-stream (stream)
-  (let ((temp-package-name "SPECIALIZATION-STORE.TESTS.TEST-STREAM"))
-    (when (find-package temp-package-name)
-      (delete-package temp-package-name))
-    (let ((*package* (make-package temp-package-name
-                                   :use '("COMMON-LISP" "SPECIALIZATION-STORE" "FIVEAM")))
-          (*print-case* :downcase)
-          (suite (make-suite 'test-stream-test)))
-      (in-suite test-stream-test)
-      (import 'syntax-layer-test *package*)
-      (import 'glue-layer-test *package*)
-      (import 'stop *package*)
-      (let ((form (read stream nil nil)))
-        (cond
-          ((null form)
-           nil)
-          ((and (symbolp form) (eql form 'stop))
-           nil)
-          ((and (listp form)
-                (member (first form) '(glue-layer-test syntax-layer-test)))
-           (format t "~%;;;; Preparing global environment for ~W ~W.~%" (second form) (first form))
-           (uiop:with-temporary-file (:stream s :pathname p)
-             (uiop:with-temporary-file (:pathname p-fasl :type (pathname-type (compile-file-pathname p)))
-               (loop
-                 for expression in (cddr form)
-                 do
-                    (pprint expression s))
+(defun output-test (form stream)
+  (let* ((*package* (find-package "SPECIALIZATION-STORE.TESTS.TEST-STREAM"))
+         (*print-case* :downcase))
+    (format stream "~&;;;; SANDBOX PREAMBLE~%")
+    (dolist (preamble-form `((defpackage "SPECIALIZATION-STORE.TESTS.TEST-STREAM"
+                               (:use "COMMON-LISP"
+                                     "SPECIALIZATION-STORE"
+                                     "FIVEAM")
+                               (:import-from "SPECIALIZATION-STORE.TESTS"
+                                             "SYNTAX-LAYER-TEST"
+                                             "GLUE-LAYER-TEST"
+                                             "STOP"))
+                             (in-package "SPECIALIZATION-STORE.TESTS.TEST-STREAM")))
+      (pprint preamble-form stream))
+    (format stream "~%")
+    (format stream "(5am:def-suite test-stream-tests)~%")
+    (format stream "(5am:in-suite test-stream-tests)~%")
+    (format stream "~&~%~%")
+    (format stream "~&;;;; ~A ~A~%" (first form) (second form))
+    (loop
+      for subform in (subseq form 2)
+      do
+         (pprint subform stream)
+         (terpri stream))
+    (terpri stream))
+  (values))
+
+(defun evaluate-test (form &key pathname (if-exists :error))
+  (let* ((test-type (string-downcase (string (first form))))
+         (test-name (string-downcase (string (second form)))))
+    (format t "~%;;;; Evaluating test: ~A ~A.~%" test-type test-name)
+    ;; Compile and load the test.
+    (flet ((compile-and-load (pathname)
+             (uiop:with-temporary-file (:stream s :pathname fasl-path :type (pathname-type (compile-file-pathname pathname)))
                (close s)
-               (with-compilation-unit (:override t)
-                 (multiple-value-bind (fasl-pathname warningsp failurep) (compile-file p :output-file p-fasl)
-                   (cond ((or warningsp failurep)
-                          (format t "~&;; Failed to compile test ~A ~A.~%" (second form) (first form)))
-                         (t
-                          (load fasl-pathname)
-                          (let* ((fiveam:*test-dribble* nil)
-                                 (report (run suite))
-                                 (succeededp (fiveam:results-status report)))
-                            (format t "~&;; Test ~A.~%"
-                                    (if succeededp
-                                        "passed"
-                                        "failed"))
-                            (unless succeededp
-                              (setf fiveam:*test-dribble* t)
-                              (fiveam:explain! report)))))))))
-           t)
-          (t
-           t))))))
+               (ignore-errors (delete-package "SPECIALIZATION-STORE.TESTS.TEST-STREAM"))
+               (multiple-value-bind (garbage warningsp failuresp)
+                   (with-compilation-unit (:override t)
+                     (compile-file pathname :output-file fasl-path))
+                 (declare (ignore garbage))
+                 (ignore-errors (delete-package "SPECIALIZATION-STORE.TESTS.TEST-STREAM"))
+                 (cond ((or warningsp failuresp)
+                        (error "Unable to compile test ~A ~A." test-type test-name))
+                       ((not (load fasl-path))
+                        (error "Unable to load test ~A ~A." test-type test-name)))))))
+      (cond (pathname
+             (with-open-file (out pathname :if-exists if-exists :direction :output)
+               (output-test form out))
+             (compile-and-load pathname))
+            (t
+             (uiop:with-temporary-file (:stream s :pathname p :direction :output)
+               (output-test form s)
+               (close s)
+               (compile-and-load p))))))
+  ;; Run the test.
+  (let* ((5am:*test-dribble* nil)
+         (report (5am:run (intern "TEST-STREAM-TESTS" "SPECIALIZATION-STORE.TESTS.TEST-STREAM")))
+         (succeededp (5am:results-status report)))
+    (format t "~&;; Sandbox test ~A.~%" (if succeededp "passed" "failed"))
+    (unless succeededp
+      (setf 5am:*test-dribble* t)
+      (5am:explain! report))
+    succeededp))
+
+(defun process-test-stream (stream)
+  (ignore-errors (delete-package "SPECIALIZATION-STORE.TESTS.TEST-STREAM"))
+  (let* ((form (let* ((*package* (make-package "SPECIALIZATION-STORE.TESTS.TEST-STREAM"
+                                                   :use '("COMMON-LISP" "SPECIALIZATION-STORE" "FIVEAM"))))
+                 (import 'glue-layer-test)
+                 (import 'syntax-layer-test)
+                 (import 'stop)
+                 (read stream nil nil))))
+    (cond
+      ((null form)
+       nil)
+      ((and (symbolp form) (eql form 'stop))
+       nil)
+      ((and (listp form)
+            (member (first form) '(glue-layer-test syntax-layer-test)))
+       (values t (evaluate-test form :pathname "/tmp/example.lisp" :if-exists :supersede)))
+      (t
+       (values t t)))))
 
 (defun process-test-streams ()
   (with-open-file (in/syntax *syntax-layer-tests-pathname*)
     (with-open-file (in/glue *glue-layer-tests-pathname*)
       (with-open-stream (in (make-concatenated-stream in/glue in/syntax))
-        (loop while (process-test-stream in))))))
+        (loop
+          with all-pass = t
+          for (continue? pass?) = (multiple-value-list (process-test-stream in))
+          while continue?
+          do
+             (unless pass?
+               (setf all-pass nil))
+          finally
+             (if all-pass
+                 (format t "~&;; All sandbox tests passed.~%")
+                 (format t "~&;; Some sandbox tests have failed.~%")))))))
